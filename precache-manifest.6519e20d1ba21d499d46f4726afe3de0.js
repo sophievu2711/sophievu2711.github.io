d27aaf6513ff2b62935049e2de7fd9f5718b130a
Nga Vu <svu2711@gmail.com>
2020-08-20 04:02:32 +1000

    Updates

---
+++ precache-manifest.6519e20d1ba21d499d46f4726afe3de0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00d5c26876443c1ef23c72ef0dede467",
+    "url": "/sophievu2711.github.io/index.html"
+  },
+  {
+    "revision": "04cfa6f00b06cae50ea7",
+    "url": "/sophievu2711.github.io/static/css/main.b8fc0567.chunk.css"
+  },
+  {
+    "revision": "ba95c88cb95cc6b42d2b",
+    "url": "/sophievu2711.github.io/static/js/2.9bcb2f5c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sophievu2711.github.io/static/js/2.9bcb2f5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04cfa6f00b06cae50ea7",
+    "url": "/sophievu2711.github.io/static/js/main.b1b119c2.chunk.js"
+  },
+  {
+    "revision": "676a9cdd7e5e786de964",
+    "url": "/sophievu2711.github.io/static/js/runtime-main.c2e1cd5a.js"
+  },
+  {
+    "revision": "d2146bebad8d4b18b7f61c0672630a37",
+    "url": "/sophievu2711.github.io/static/media/adjust.d2146beb.png"
+  },
+  {
+    "revision": "d2d3365f36dedf951704f0146fcf469c",
+    "url": "/sophievu2711.github.io/static/media/profile3.d2d3365f.png"
+  },
+  {
+    "revision": "ef5bde7e306ebe9921c6fc26de32d611",
+    "url": "/sophievu2711.github.io/static/media/sg.ef5bde7e.png"
+  }
+]);
