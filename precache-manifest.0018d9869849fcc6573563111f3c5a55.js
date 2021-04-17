137b2e0677fc0556fbd71363e9b88db5a66856ed
Nga Vu (Sophie) <svu2711@gmail.com>
2021-04-18 01:17:17 +1000

    Updates

---
+++ precache-manifest.0018d9869849fcc6573563111f3c5a55.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f503339164f10a92ca458dd4d91f849",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06f00cad8bebd8707d2f",
+    "url": "/static/css/main.b8fc0567.chunk.css"
+  },
+  {
+    "revision": "1806b9041ad3509a3808",
+    "url": "/static/js/2.ebc26934.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ebc26934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06f00cad8bebd8707d2f",
+    "url": "/static/js/main.578b7d4f.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "d2146bebad8d4b18b7f61c0672630a37",
+    "url": "/static/media/adjust.d2146beb.png"
+  },
+  {
+    "revision": "d2d3365f36dedf951704f0146fcf469c",
+    "url": "/static/media/profile3.d2d3365f.png"
+  },
+  {
+    "revision": "ef5bde7e306ebe9921c6fc26de32d611",
+    "url": "/static/media/sg.ef5bde7e.png"
+  }
+]);
