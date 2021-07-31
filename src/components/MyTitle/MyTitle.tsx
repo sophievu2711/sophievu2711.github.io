cff921a468419cdc0cc233d4ddfc0a9b9c3ca78e
Sophie <sophie@truescope.com>
2021-08-01 00:07:03 +1000

    update profile

--- src/components/MyTitle/MyTitle.tsx
+++ src/components/MyTitle/MyTitle.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import TitlePagination from './TitlePagination';
-import Fadein from '../../common/effect/Fadein';
+import React from "react";
+import TitlePagination from "./TitlePagination";
+import Fadein from "../../common/effect/Fadein";
 
 const myTitles = [
-  "A creative web developer with a focus on frontend",
+  "A creative frontend engineer",
   "A business systems analyst",
   "An UI/UX designer",
 ];
@@ -12,50 +12,53 @@ const MyTitle: React.FC = () => {
   const [index, setIndex] = React.useState(2);
   const handleChangeIndex = (idx: number) => {
     setIndex(idx);
-  }
+  };
   React.useEffect(() => {
     setTimeout(() => {
-      switch(index){
-        case 0: setIndex(1); break;
-        case 1: setIndex(2); break;
-        case 2: setIndex(0); break;
+      switch (index) {
+        case 0:
+          setIndex(1);
+          break;
+        case 1:
+          setIndex(2);
+          break;
+        case 2:
+          setIndex(0);
+          break;
       }
-    }, 4000)
-    console.log(index);
-  })
+    }, 4000);
+  });
   const styles = {
     slide: {
-      color: '#fff',
-      alignItems: 'right',
-      justifyContent: 'center',
-      display: 'flex',
-      fontFamily: 'Julius Sans One',
-      fontSize: '2vw',
+      color: "#fff",
+      alignItems: "right",
+      justifyContent: "center",
+      display: "flex",
+      fontFamily: "Julius Sans One",
+      fontSize: "2vw",
     },
   };
   return (
     <React.Fragment>
-      {
-        myTitles.map(
-          (title, i) => (
-            (i === index) && 
-              <Fadein>
-                <span style={styles.slide}>{title}</span>
-              </Fadein>
+      {myTitles.map(
+        (title, i) =>
+          i === index && (
+            <Fadein>
+              <span style={styles.slide}>{title}</span>
+            </Fadein>
           )
-        )
-      }
-      <div style={{ flexDirection: 'row' }}>
-        {
-          Array(myTitles.length).fill(0).map((_, i) => (
+      )}
+      <div style={{ flexDirection: "row" }}>
+        {Array(myTitles.length)
+          .fill(0)
+          .map((_, i) => (
             <span onClick={() => handleChangeIndex(i)}>
-              <TitlePagination selected={i === index}/>
+              <TitlePagination selected={i === index} />
             </span>
-          ))
-        }        
+          ))}
       </div>
     </React.Fragment>
   );
-}
+};
 
 export default MyTitle;
