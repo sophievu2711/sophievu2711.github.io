efdec20050e4dce928ead61e246b8651856ce9b9
Sophie <sophie@truescope.com>
2021-07-31 19:58:34 +1000

    Updates

---
+++ precache-manifest.6e1a04e58cf09ed00ffbbce4f9c5208d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c1605109405800e02b4b2bd2d6ad06c",
+    "url": "/sophievu2711.github.io/index.html"
+  },
+  {
+    "revision": "6b7f9142d9da21b330ec",
+    "url": "/sophievu2711.github.io/static/css/main.b8fc0567.chunk.css"
+  },
+  {
+    "revision": "4346ca0d088d4908906a",
+    "url": "/sophievu2711.github.io/static/js/2.5fd27afc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sophievu2711.github.io/static/js/2.5fd27afc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b7f9142d9da21b330ec",
+    "url": "/sophievu2711.github.io/static/js/main.0b065901.chunk.js"
+  },
+  {
+    "revision": "676a9cdd7e5e786de964",
+    "url": "/sophievu2711.github.io/static/js/runtime-main.c2e1cd5a.js"
+  },
+  {
+    "revision": "d2146bebad8d4b18b7f61c0672630a37",
+    "url": "/sophievu2711.github.io/static/media/adjust.d2146beb.png"
+  },
+  {
+    "revision": "d2d3365f36dedf951704f0146fcf469c",
+    "url": "/sophievu2711.github.io/static/media/profile3.d2d3365f.png"
+  },
+  {
+    "revision": "ef5bde7e306ebe9921c6fc26de32d611",
+    "url": "/sophievu2711.github.io/static/media/sg.ef5bde7e.png"
+  }
+]);
